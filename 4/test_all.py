fad51e06db66044ce8b8e5aac40a932d6a16a6d7
Robrecht De Rouck <de.rouck.robrecht@gmail.com>
2018-10-14 22:26:49 +0100

    Add comment about test reliability.

--- 4/test_all.py
+++ 4/test_all.py
@@ -8,6 +8,9 @@ from mytool import extract, extract_single
 
 def test_extract_single():
     extracted = extract_single("http://google.com")
+    # Image path probably pretty stable; ideally we extract from a page 
+    # that never changes like a self-hosted testing page set up for this
+    # purpose.
     assert re.findall("http://google.com/images.*.png", extracted["images"][0])
 
     for link in extracted["links"]:
